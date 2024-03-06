4c875172b3288860f192d9be13cc26a090563dcb
kurnik <c.jaworski@fu-berlin.de>
2024-03-06 23:09:21 +0100

    feat: Update routes.rb to include new routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -7,6 +10,6 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  root to: 'home#index'
+  root to: 'posts#index'
+  resources :posts, only: %i[new create index]
 end
